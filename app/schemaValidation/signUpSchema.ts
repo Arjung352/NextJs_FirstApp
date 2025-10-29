35e3fde211f2c5eb632c03c0aa03f28691dfced3
Arjun gupta <rachnag352@gmail.com>
2025-10-29 22:18:15 +0530

    started schemaValidation using zod

---
+++ app/schemaValidation/signUpSchema.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const userNameValidation = z
+  .string()
+  .min(3, { message: "Username should be at least 3 characters long" })
+  .max(20, { message: "Username should not exceed 20 characters" })
+  .regex(/^[A-Za-z0-9 _-]+$/, {
+    message:
+      "Username must not contain special characters other than '-' or '_'",
+  });
+
+export const signUpSchema = z.object({
+  username: userNameValidation,
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password should be at least 6 characters long" }),
+});
