e4841deaa96b7f443425b9fb158e59ca01c82166
Arjun gupta <rachnag352@gmail.com>
2025-11-26 01:19:11 +0530

    completed ui for sign up page and implemented debouncing in it

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,19 +1,7 @@
-"use client";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import Link from "next/link";
-import { useState } from "react";
-import { useDebounceValue } from 'usehooks-ts'
+import React from "react";
 
-export function LoginForm() {
-const [username,setUsername]=useState('');
-const [usernameMessage,setUsernameMessage]=useState('')
-const [loading,setLoading]=useState(false)
-const [debouncedValue, setValue] = useDebounceValue(username, 300)
-
-const [isSubmitting,setIsSubmitting]=useState(false)
-  return (
-    
-  );
+function page() {
+  return <div>sign in page</div>;
 }
+
+export default page;
