c17254284c143604ab45c86f846841a0983a14db
Arjun gupta <rachnag352@gmail.com>
2025-11-25 23:56:26 +0530

    implementing shadcn and debouncing

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useState } from "react";
+import { useDebounceValue } from 'usehooks-ts'
 
-function page() {
-  return <div>page</div>;
+export function LoginForm() {
+const [username,setUsername]=useState('');
+const [usernameMessage,setUsernameMessage]=useState('')
+const [loading,setLoading]=useState(false)
+const [debouncedValue, setValue] = useDebounceValue(username, 300)
+
+const [isSubmitting,setIsSubmitting]=useState(false)
+  return (
+    
+  );
 }
-
-export default page;
